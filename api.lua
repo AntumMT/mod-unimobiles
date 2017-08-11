bf2207c028c1de13a0d34c972fd0e6633777610b
Jordan Irwin <antumdeluge@gmail.com>
2017-08-10 19:23:24 -0700

    Remove 'compatibility' functions

--- api.lua
+++ api.lua
@@ -26,55 +26,35 @@ function umobs.mobsRegisterMob(name, def)
 	return mobs:register_mob(name, def)
 end
 
---- Compatibility function.
---
--- @function umobs.mobs_register_mob
--- @param name
--- @param def
--- @see umobs.mobsRegisterMob
-umobs.mobs_register_mob = umobs.mobsRegisterMob
-
 
 function umobs.mobsRegisterSpawn(name, nodes, max_light, min_light, chance, active_object_count, max_height, day_toggle)
 	return mobs:register_spawn(name, nodes, max_light, min_light, chance, active_object_count, max_height, day_toggle)
 end
-
-umobs.mobs_register_spawn = umobs.mobsRegisterSpawn()
 
 
 function umobs.mobsRegisterSpawnSpecific(name, nodes, neighbors, min_light, max_light, interval, chance, active_object_count, min_height, max_height, day_toggle, on_spawn)
 	return mobs:spawn_specfic(name, nodes, neighbors, min_light, max_light, interval, chance, active_object_count, min_height, max_height, day_toggle, on_spawn)
 end
 
-umobs.mobs_spawn_specific = umobs.mobsRegisterSpawnSpecific
-
 
 function umobs.mobsRegisterArrow(name, def)
 	return mobs:register_arrow(name, def)
 end
-
-umobs.mobs_register_arrow = umobs.mobsRegisterArrow
 
 
 function umobs.mobsRegisterEgg(name, description, background, addegg)
 	return mobs:register_egg(name, description, background, addegg)
 end
 
-umobs.mobs_register_egg = umobs.mobsRegisterEgg
-
 
 function umobs.mobsBoom(self, pos, radius)
 	return mobs:boom(self, pos, radius)
 end
 
-umobs.mobs_boom = umobs.mobsBoom
-
 
 function umobs.mobsCaptureMob(self, clicker, chance_hand, chance_net, chance_lasso, force_take, replacewith)
 	return mobs:capture_mob(self, clicker, chance_hand, chance_net, chance_lasso, force_take, replacewith)
 end
-
-umobs.mobs_capture_mob = umobs.mobsCaptureMob
 
 
 function umobs.mobsFeedTame(self, clicker, feed_count, breed, tame)
